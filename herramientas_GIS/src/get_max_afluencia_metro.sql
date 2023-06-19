c3cf13af2f1d9f2e49f25ef77e1c7ecf8ac56835
MaritzaB <amby.121192@gmail.com>
2023-06-19 12:51:19 -0600

    💫 Update query

--- herramientas_GIS/src/get_max_afluencia_metro.sql
+++ herramientas_GIS/src/get_max_afluencia_metro.sql
@@ -1,7 +1,9 @@
--- Obtiene la estación del metro con máxima afluencia en una fecha dada.
+-- Obtiene la estación del metro con máxima afluencia en una fecha dada y un
+-- buffer al rededor de 800 metros de radio.
 
 select m.fecha, m.max_afluencia, t.estacion, getLn(t.linea) as linea,
-	em.id, em.geom
+	em.id, geom as st_geom,
+	st_transform(st_buffer(st_transform(em.geom,32614),800),4326)  as geom
 	from (select sq.fecha, max(sq.total_afluencia) as max_afluencia
 	from(
 		select fecha, linea, estacion, sum(afluencia) as total_afluencia
@@ -13,9 +15,10 @@ join (
 	select fecha, linea, estacion, sum(afluencia) as total_afluencia
 	from clase_afluencia_stc cas 
 	group by fecha, linea, estacion) t
-on t.fecha = m.fecha and t.total_afluencia = m.max_afluencia
-join
-  "STC_Metro_estaciones_utm14n" em
 on
-  getLn(t.linea) = em.linea and t.estacion = em.nombre
+	t.fecha = m.fecha and t.total_afluencia = m.max_afluencia
+join
+	"STC_Metro_estaciones_utm14n" em
+on
+	getLn(t.linea) = em.linea and t.estacion = em.nombre
 ;
